fc70a5ac20f76846ecf7dc609c4c5fae4ca38e3b
prof30 <mine4christ@gmail.com>
2020-03-07 14:35:47 -0800

    dashboard blade created

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,11 +17,25 @@ class RedirectIfAuthenticated
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
-    {
-        if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
-        }
+        {
+            if ($guard == "admin" && Auth::guard($guard)->check()) {
+                return redirect('/admin');
+            }
+            if ($guard == "writer" && Auth::guard($guard)->check()) {
+                return redirect('/writer');
+            }
+            if (Auth::guard($guard)->check()) {
+                return redirect('/home');
+            }
 
-        return $next($request);
-    }
+            return $next($request);
+        }
+    // public function handle($request, Closure $next, $guard = null)
+    // {
+    //     if (Auth::guard($guard)->check()) {
+    //         return redirect(RouteServiceProvider::HOME);
+    //     }
+
+    //     return $next($request);
+    // }
 }
